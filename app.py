81508673b0c0c4f95a40c784ea6cb54c20d3e623
homelyseven250 <georgee.keylock@outlook.com>
2021-07-21 11:23:27 +0100

    Add password protection

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 # An application using to serve files if the password for each file is correct
 
 from flask import Flask, render_template, request, send_from_directory, abort, redirect, url_for
-import os
-import hashlib
+import os, json, bcrypt
 
 # Initialize the Flask application
 app = Flask(__name__)
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-# These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(
-    ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-# For a given file, return whether it's an allowed type or not
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-# Index page
-
 
 @app.route('/')
 def index():
@@ -35,15 +22,23 @@ def download():
         return render_template('download.html')
     # If the request was a post request, find the file in the upload directory by hash and send it to the client
     elif request.method == 'POST':
-        # Get the name of the file to be downloaded
-        file_hash = request.form['file_hash']
-        # Loop through the files in the upload directory and find the file with the matching hash
-        for file in os.listdir(app.config['UPLOAD_FOLDER']):
-            # If the file has the hash, send it to the client
-            if file_hash == hashlib.sha256(open(app.config['UPLOAD_FOLDER'] + file, 'rb').read()).hexdigest():
-                return send_from_directory(app.config['UPLOAD_FOLDER'], file, as_attachment=True)
-        # If the file was not found, return an error message
-        abort(418)
+        file_name = request.form['file_name']
+        # check if the file exists
+        if os.path.isdir(app.config['UPLOAD_FOLDER'] + file_name):
+            #check if the password is correct from the json file
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name, 'data.json')) as json_file:
+                data = json.load(json_file)
+                #check if the password is correct
+                if bcrypt.checkpw(request.form['password'].encode('utf-8'), data['password']):
+                    #send the file to the client
+                    return send_from_directory(app.config['UPLOAD_FOLDER'] + file_name, file_name, as_attachment=True)
+                else:
+                    abort(403)
+        else:
+            abort(404)
+    else:
+        abort(405)
+
 
 # Upload page
 
@@ -59,9 +54,15 @@ def upload():
 
         file = request.files['file']
         if file:
-            if os.path.isfile(app.config['UPLOAD_FOLDER'] + file.filename):
+            if os.path.isdir(app.config['UPLOAD_FOLDER'] + file.filename):
                 abort(409)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            return hashlib.sha256(open(app.config['UPLOAD_FOLDER'] + file.filename, 'rb').read()).hexdigest() + '''
-            <a href="download"> Go to the download page - make sure to copy your unique hash.</a>'''
-    abort(418)
+            else:
+                os.mkdir(app.config['UPLOAD_FOLDER'] + file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename, file.filename))
+                with open(os.path.join(app.config['UPLOAD_FOLDER'], file.filename, "data.json"), 'w') as dataJson:
+                    json.dump({"password":bcrypt.hashpw(request.form['password'], bcrypt.gensalt(12)), "fileName":file.filename}, dataJson)
+                return redirect(url_for('upload'))
+        else:
+            abort(400)
+    else:
+        abort(405)
