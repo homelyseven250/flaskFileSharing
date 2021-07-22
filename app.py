f0ba627244828cfff03090fffa5863d691a1956e
homelyseven250 <georgee.keylock@outlook.com>
2021-07-22 03:47:33 +0100

    Add working link generation. Improved some CSS.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # An application using to serve files if the password for each file is correct
 
 from flask import Flask, render_template, request, send_from_directory, abort, redirect, url_for
-import os, json, bcrypt
+import os, json, bcrypt, secrets, hashlib
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -60,9 +60,55 @@ def upload():
                 os.mkdir(app.config['UPLOAD_FOLDER'] + file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename, file.filename))
                 with open(os.path.join(app.config['UPLOAD_FOLDER'], file.filename, "data.json"), 'w') as dataJson:
-                    json.dump({"password":bcrypt.hashpw(request.form['password'], bcrypt.gensalt(12)), "fileName":file.filename}, dataJson)
+                    json.dump({"password":bcrypt.hashpw(request.form['password'], bcrypt.gensalt(12)), "fileName":file.filename, "browserAuth":secrets.token_urlsafe(1024)}, dataJson)
+
                 return redirect(url_for('upload'))
         else:
             abort(400)
     else:
         abort(405)
+
+# Download a file from a link 
+@app.route("/download/<file_name>/<hash>/<password>")
+def download_link(file_name, hash, password):
+    #check if the file exists
+    if os.path.isdir(app.config['UPLOAD_FOLDER'] + file_name):
+        #check if the password is correct from the json file
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name, 'data.json')) as json_file:
+            data = json.load(json_file)
+            #check if the password is correct
+            print(password == data['browserAuth'])
+            # Hash the file
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name, file_name), 'rb') as file:
+                bytes = file.read()
+                fileHash = hashlib.sha256(bytes).hexdigest()
+
+            print(fileHash)
+            print(hash)
+            if password == data['browserAuth'] and hash == fileHash:
+                return send_from_directory(app.config['UPLOAD_FOLDER'] + file_name, file_name, as_attachment=True)
+            else:
+                abort(403)
+    else:
+        abort(404)
+
+# Generate a file download link
+@app.route("/generate", methods=['GET', 'POST'])
+def generate():
+    if request.method == 'GET':
+        return render_template('generate.html')
+    elif request.method == 'POST':
+        # Get the name of the file to be uploaded
+        file_name = request.form['file_name']
+        # Check if the password is correct from the json file
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name, 'data.json')) as json_file:
+            data = json.load(json_file)
+            #check if the password is correct
+            if bcrypt.checkpw(request.form['password'].encode('utf-8'), data['password']):
+                # Hash the file
+                with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name, file_name), 'rb') as file:
+                    bytes = file.read()
+                    fileHash = hashlib.sha256(bytes).hexdigest()
+                # Generate a link to the file
+                link = "http://127.0.0.1:5000/download/" + file_name + "/" + fileHash + "/" + data['browserAuth']
+                return render_template('generate.html', link=link)
